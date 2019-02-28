fc820e1446acbac2dc3ff094f40fb57778e76f13
unknown <j.veitchmichaelis@gmail.com>
2019-02-28 20:22:12 +0000

    update classes

--- main.py
+++ main.py
@@ -11,44 +11,66 @@ def grouper(n, iterable, fillvalue=None):
     args = [iter(iterable)] * n
     return itertools.zip_longest(*args, fillvalue=fillvalue)
 
-class photo:
-    def __init__(self, tags, orientation):
-        self.tags = set(tags)
-        self.orientation = orientation
-
-def transition_score(current_photo, next_photo):
-    score_1 = len(current_photo.tags.union(next_photo.tags))
-    score_2 = len(current_photo.tags.difference(next_photo.tags))
-    score_3 = len(next_photo.tags.difference(current_photo.tags))
+def transition_score(tags_a, tags_b):
+    score_1 = len(tags_a.union(tags_b))
+    score_2 = len(tags_a.difference(tags_b))
+    score_3 = len(tags_b.difference(tags_a))
 
     return min(score_1, score_2, score_3)
 
+class photo:
+    def __init__(self, id, tags, orientation = "H"):
+        self.tags = set(tags)
+        self.index = id
+        self.orientation = orientation
+
+class slide:
+    def __init__(self, photos):
+        self.tags = set()
+        self.photos = photos
+
+        for p in self.photos:
+            self.tags = self.tags.union(p.tags)
+
+    def transition_score(self, next_slide):
+        return transition_score(self.tags, next_slide.tags)
+
 class slideshow:
-    def __init__(self):
-        self.photos = []
+    def __init__(self, slides = None):
+        self.slides = []
         self.score = 0
 
-    def add_photos(self, photos):
-        for photo in photos:
-            self.add_photo(photo)
+        if slides is not None:
+            self.add_slides(slides)
     
-    def add_photo(self, _photo):
-        assert(photo not in self.photos)
+    def add_slides(self, slides):
+        for s in slides:
+            self.add_slide(s)
+    
+    def add_slide(self, next_slide):
 
-        if len(self.photos) == 0:
-            prev_slide = photo([], None)
+        if len(next_slide.photos) == 0:
+            return
+
+        if len(self.slides) == 0:
+            prev_slide = slide([])
         else:
-            prev_slide = self.photos[-1]    
+            prev_slide = self.slides[-1]
         
-        next_slide = _photo
+        self.score += prev_slide.transition_score(next_slide)
+        self.slides.append(next_slide)
 
-        self.score += transition_score(prev_slide, next_slide)
-        
-        self.photos.append(_photo)
+    def save(self, filename):
 
-
-def merge_vertical_photos(photos):
-    pass
+        with open(filename, "w") as of:
+            of.write("{}\n".format(len(self.slides)))
+            for slide in self.slides:
+                ids = []
+                for photo in slide.photos:
+                    ids.append(photo.index)
+                
+                of.write(" ".join([str(id) for id in ids]))
+                of.write("\n")
 
 def generate_similarity_matrix(photos):
 
@@ -66,26 +88,28 @@ def parse_input(input_filename):
 
     photos = []
 
-    for meta in data[1:]:
+    for i, meta in enumerate(data[1:]):
         meta = meta.split(" ")
 
         orientation = meta[0]
         tags = meta[2:]
 
-        photos.append(photo(tags, orientation))
+        photos.append(photo(i, tags, orientation))
 
     return photos
 
-def optimal_subsets(photos, n=10):
+def optimal_subsets(slides, n=5):
 
     slideshows = []
 
-    for subset in grouper(10, photos):
+    for subset in tqdm(grouper(n, slides, slide([]))):
+
         best_score = 0
 
-        for combo in itertools.combinations(subset, 10):
+        for permutation in itertools.permutations(subset, n):
             s = slideshow()
-            s.add_photos(combo)
+
+            s.add_slides(permutation)
 
             if s.score >= best_score:
                 best_score = s.score
@@ -95,16 +119,38 @@ def optimal_subsets(photos, n=10):
 
     return slideshows
 
+def plot_similarity_matrix(similarity):
+    plt.imshow(similarity)
+    plt.colorbar()
+    plt.show()
+
+def merge_slideshows(slideshows):
+
+    master_show = slideshow()
+
+    for s in slideshows:
+        master_show.add_slides(s.slides)
+
+    return master_show
+
 if __name__ == "__main__":
     photos = parse_input(sys.argv[1])
 
     random.seed(42)
     random.shuffle(photos)
 
-    photo_subset = photos[:100]
-    similarity = generate_similarity_matrix(photo_subset)
-    optimal_subsets(photo_subset)
+    photos = photos
+    slides = [slide([photo]) for photo in photos]
 
-    plt.imshow(similarity)
-    plt.colorbar()
-    plt.show()
+    slideshows = optimal_subsets(slides, 6)
+
+    # Calculate reference slideshow score
+    ref_slideshow = slideshow(slides)
+    print("Reference (random) slideshow score: ", ref_slideshow.score)
+
+    combined = merge_slideshows(slideshows)
+
+    print("Combined score: ", combined.score)
+
+    combined.save("output.txt")
+

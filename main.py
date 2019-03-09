37c2ae41d32332c8537bcd147246c85bab69f00c
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2019-03-09 02:18:58 +0000

    vertical greedy strategy working

--- main.py
+++ main.py
@@ -14,13 +14,10 @@ from slideshow import Slideshow
 
 input_file = sys.argv[1]
 
-# From Python cookboook
-def grouper(n, iterable, fillvalue=None):
-    "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
-    return itertools.zip_longest(*args, fillvalue=fillvalue)
-
 def generate_similarity_matrix(photos):
+    """
+    Convenience function to generate an adjacency matrix between all photos
+    """
 
     similarity = np.zeros((len(photos),len(photos)), dtype='uint8')
 
@@ -54,41 +51,6 @@ def parse_input(input_filename):
         photos.append(Photo(i, tags, orientation))
 
     return photos
-
-def optimal_subsets(slides, n=5):
-
-    slideshows = []
-
-    for subset in tqdm(grouper(n, slides, Slide())):
-
-        best_score = 0
-
-        for permutation in itertools.permutations(subset, n):
-            s = Slideshow()
-
-            s.add_slides(permutation)
-
-            if s.score >= best_score:
-                best_score = s.score
-                best_s = s
-
-        slideshows.append(best_s)
-
-    return slideshows
-
-def optimal_subset_slideshow(photos):
-    slides = get_horizontal_slides(photos) + get_vertical_slides(photos)
-    slideshows = optimal_subsets(slides, 6)
-    return  merge_slideshows(slideshows)
-
-def merge_slideshows(slideshows):
-
-    merged_slideshow = Slideshow()
-
-    for slideshow in slideshows:
-        merged_slideshow.add_slides(slideshow.slides)
-
-    return merged_slideshow
 
 def find_greedy_match_slide(slide, slides):
     """
@@ -143,28 +105,35 @@ def find_greedy_match_photo(slide, photos):
 
     return best_match, best_score
 
-def find_greedy_match_vertical_photo(slide, next_slide, photos):
+def find_greedy_match_vertical_photo(slide, photo, photos):
+    """
+    Find the best vertical photo
+    """
+
     if len(photos) == 0:
         return 0, -1
         
+    best_possible = len(slide.tags)
     best_score = 0
     best_match = random.randint(0, len(photos)-1)
 
     for i, p in enumerate(photos):
 
-        comparison_slide = Slide(next_slide.photos[0], p)
-        score = slide.score(comparison_slide)
+        score = slide.score(Slide(photo, p))
 
         if score > best_score:
             best_score = score
             best_match = i
+
+            if score == best_possible:
+                break
         
     return best_match, best_score
 
 def get_horizontal_slides(photos):
     return [Slide(p) for p in photos if p.orientation == "H"]
 
-def get_vertical_slides_simple(photos, rand_seed=time.time()):
+def get_vertical_slides(photos, rand_seed=time.time()):
 
     random.seed(rand_seed)
     random.shuffle(photos)
@@ -178,7 +147,7 @@ def get_vertical_slides_simple(photos, rand_seed=time.time()):
     return slides
 
 def get_slides(photos):
-    return get_horizontal_slides(photos) + get_vertical_slides_simple(photos)
+    return get_horizontal_slides(photos) + get_vertical_slides(photos)
 
 def greedy_slideshow_slides(slides, rand_seed=time.time()):
     random.seed(rand_seed)
@@ -218,12 +187,8 @@ def greedy_slideshow(photos, rand_seed=time.time()):
 
     if n_h == 0:
         show = Slideshow([ Slide(vertical_photos.pop(), vertical_photos.pop()) ])
-    elif n_v == 0:
-        # Don't care, just take any
-        show = Slideshow([ Slide(horizontal_photos.pop()) ])
     else:
-        #Take a horizontal photo, the remaining order doesn't matter
-        show = Slideshow([ Slide(horizontal_photos.pop(-1)) ])
+        show = Slideshow([ Slide(horizontal_photos.pop()) ])
            
     n_slides = len(horizontal_photos) + len(vertical_photos)//2
 
@@ -239,26 +204,19 @@ def greedy_slideshow(photos, rand_seed=time.time()):
         best_idx_h, score_h = find_greedy_match_photo(show.slides[-1], horizontal_photos)
         best_idx_v, score_v = find_greedy_match_photo(show.slides[-1], vertical_photos)
 
-        if score_h == score_v:
-            if random.random() > 0.5:
-                score_h += 1e4
-
         if score_h > score_v:
             # Next slide should be horizontal
             show.add_slide(Slide(horizontal_photos.pop(best_idx_h)))
         else:
             # Next slide has vertical images
-            show.add_slide(Slide(vertical_photos.pop(best_idx_v)))
+            best_photo = vertical_photos.pop(best_idx_v)
 
             # Find the next best vertical image to include
-            #best_idx, _ = find_greedy_match_vertical_photo(show.slides[-2], show.slides[-1], vertical_photos)
-            show.slides[-1].add_photo(vertical_photos.pop(0))
+            best_idx, _ = find_greedy_match_vertical_photo(show.slides[-1], best_photo, vertical_photos)
+            show.add_slide(Slide(best_photo, vertical_photos.pop(best_idx)))
 
-        if i % 10000 == 0:
-            os.makedirs("./results", exist_ok=True)
-            show.save("./results/{}_{}_{}.txt".format(os.path.splitext(os.path.basename(input_file))[0], show.score, time.time()))
-
-        pbar.set_postfix(score=show.score, n_slides=len(show.slides), photos_remaining=total_photos)
+        avg_score = show.score/len(show.slides)
+        pbar.set_postfix(score=show.score, avg_score=avg_score)
 
     return show
 
@@ -273,14 +231,14 @@ if __name__ == "__main__":
             similarity = generate_similarity_matrix(photos)
             np.save("./results/similarity_{}".format(base_name), similarity)
 
-    # Calculate reference slideshow score
-    #ref_slideshow = Slideshow(slides)
-    #print("Reference (random) slideshow score: ", ref_slideshow.score)
+    # Get slides and generate a slideshow
+    
+    # Fast, scores about 1M
+    #slides = get_vertical_slides_simple(photos) + get_horizontal_slides(photos)
+    #show = greedy_slideshow_slides(slides)
 
-    slides = get_vertical_slides_simple(photos) + get_horizontal_slides(photos)
-    show = greedy_slideshow_slides(slides)
-
-    #show = greedy_slideshow(photos, rand_seed=time.time())
+    # Abit slower, scores 1.2M
+    show = greedy_slideshow(photos)
 
     print("Slideshow score: ", show.score)
 
